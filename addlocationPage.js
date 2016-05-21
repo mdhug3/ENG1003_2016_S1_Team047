949d391c043b1b9fb13800e4cc1cf056338978ad
markthomas1997 <metho8@student.monash.edu>
2016-05-21 18:20:39 +1000

    Update addlocationPage.js
    Added the feature of searching with each new character input.
    Still requires the ability to save locations.

--- addlocationPage.js
+++ addlocationPage.js
@@ -1,31 +1,87 @@
 // Code for the Add Location page.
 ////////////////////////////////////////////////
+// Note: This example requires that you consent to location sharing when
+// prompted by your browser. If you see the error "The Geolocation service
+// failed.", it means you probably did not give permission for the browser to
+// locate you.
+
+/*function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+  });
+  var infoWindow = new google.maps.InfoWindow({map: map});
+
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
+
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    console.log();
+}
+
+*/
+
 function iMap() {
-    //call the google map
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 12,
-    center: {lat: -34.397, lng: 150.644}//set the center, as the center is changed by the user's idea, so just set any      coordinates.
+    center: {lat: -34.397, lng: 150.644}
   });
   var geocoder = new google.maps.Geocoder();
+  var infowindow = new google.maps.InfoWindow;
+
+
   document.getElementById('address').addEventListener('click', function() {
-  geocodeAddress(geocoder, map);//call the geocoder and map function
+    geocodeAddress(geocoder, map, true, infowindow);
+  });
+  document.getElementById('Address').addEventListener('input', function() {
+    geocodeAddress(geocoder, map, false);
   });
 }
 
-function geocodeAddress(geocoder, resultsMap) {
-  var address = document.getElementById("Address").value;
+function geocodeAddress(geocoder, resultsMap, isClicked, infowindow) {
+  var address = document.getElementById('Address').value;
   geocoder.geocode({'address': address}, function(results, status) {
-    if (status === google.maps.GeocoderStatus.OK) {
+    if (status === google.maps.GeocoderStatus.OK && isClicked == true) {
+      isClicked = false;
       resultsMap.setCenter(results[0].geometry.location);
+      console.log(results[0].formatted_address)
       var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location//get the position we asked in the addlocation webpage.
+        map: resultsMap,
+        position: results[0].geometry.location
       });
-          var string = JSON.stringify(results[0].geometry.location)//transfer the data into the sting style
-          localStorage.setItem("address",string)//store the string we get into the local storage
+      infowindow.setContent(results[0].formatted_address);
+      var string = JSON.stringify(results[0].geometry.location)//transfer the data into the sting style
+      localStorage.setItem("address",string)//store the string we get into the local storage
+      //infowindow.open(map, marker);
     }
-      else {
-      alert('Geocode was not successful for the following reason: ' + status);//In case any situation appear.
+    else if(status === google.maps.GeocoderStatus.OK && isClicked == false){
+        resultsMap.setCenter(results[0].geometry.location);
+        //var marker = new google.maps.Marker({
+          //map: resultsMap,
+          //position: results[0].geometry.location
+      //});
+    }
+    else {
+      if(isClicked === true){
+        alert('Geocode was not successful for the following reason: ' + status);
+      }
     }
   });
 }
@@ -49,5 +105,4 @@ function getstorage (){
     result2 = JSON.parse(str2);
     output+= result1 + "</br>" + result2 + "</br>";
 }
-
 
