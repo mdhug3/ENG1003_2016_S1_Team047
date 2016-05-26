886ddaf30f9f11a070d382145fc3e9e2e75f804e
mdhug3 <mdhug3@student.monash.edu>
2016-05-26 16:01:20 +1000

    Update viewLocationPage.js

--- viewLocationPage.js
+++ viewLocationPage.js
@@ -14,25 +14,54 @@ if (locationIndex !== null)
 var map;
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: -34.397, lng: 150.644},
-    zoom: 8
+    center: {lat: -37.9116, lng: 145.1340},
+    zoom: 13
+  });
+    var marker = new google.maps.Marker({
+    position: {lat: -37.9116, lng: 145.1340},
+    map: map,
+    title: "uni"
   });
 }
 
 this.weatherinformation = function(day){
 if(day.currently.icon === "rain"){
-    document.getElementById("icon").innerHTML= "<i class='wi wi-day-cloudy'></i>"
-    document.getElementById("sky").innerHTML= "rain"
-}
+    var image = document.getElementById("icon").innerHTML ;
     
-   
+    document.getElementById("sky").innerHTML= "Current Weather: Rain";
+} 
 if(day.currently.icon === "partly-cloudy-day"){
-    document.getElementById("icon").innerHTML= "<i class='wi wi-day-cloudy'></i>"
-    document.getElementById("sky").innerHTML= "rain"
+    document.getElementById("icon").innerHTML= ""
+    document.getElementById("sky").innerHTML= "Current Weather: Partly Cloudy Day"
 }
-document.getElementById("maxtemp").innerHTML= "Minimum Temperature:" + day.daily.data[0].temperatureMax;
+if(day.currently.icon === "partly-cloudy-night"){
+    document.getElementById("icon").innerHTML= ""
+    document.getElementById("sky").innerHTML= "Current Weather: Partly Cloudy Night"
+}
+if(day.currently.icon === "wind"){
+    document.getElementById("icon").innerHTML= ""
+    document.getElementById("sky").innerHTML= "Current Weather: Wind"
+}
+if(day.currently.icon === "sleet"){
+    document.getElementById("icon").innerHTML= '<img  scr="sleet.png">'
+    document.getElementById("sky").innerHTML= "Current Weather: Sleet"
+}
+if(day.currently.icon === "fog"){
+    document.getElementById("icon").innerHTML= ""
+    document.getElementById("sky").innerHTML= "Current Weather: Fog"
+}
+if(day.currently.icon === "clear-night"){
+    document.getElementById("icon").innerHTML= ""
+    document.getElementById("sky").innerHTML= "Current Weather: Clear Night";
+}
+if(day.currently.icon === "snow"){
+    document.getElementById("icon").innerHTML= "";
+    document.getElementById("sky").innerHTML= "Current Weather: Snow";
+}
+
+    
+document.getElementById("maxtemp").innerHTML= "Maximum Temperature:" + day.daily.data[0].temperatureMax;
 document.getElementById("mintemp").innerHTML= "Minimum tempertature:" + day.daily.data[0].temperatureMin;
-document.getElementById("wind").innerHTML = "Windspeed:"+ day.currently.windSpeed
-document.getElementById("humidity").innerHTML = "Humidity: " + day.currently.humidity + " %"
-document.getElementById("icon").innerHTML
+document.getElementById("wind").innerHTML = "Windspeed:"+ day.currently.windSpeed;
+document.getElementById("humidity").innerHTML = "Humidity: " + day.currently.humidity + " %";
 }
