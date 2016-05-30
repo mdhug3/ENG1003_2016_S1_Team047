c5fc239e099dc2c0f77fa1c985d342fa1290bba3
markthomas1997 <metho8@student.monash.edu>
2016-05-30 14:21:52 +1000

    Update viewLocationPage.js

--- viewLocationPage.js
+++ viewLocationPage.js
@@ -2,7 +2,7 @@
 
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
-//global varibles
+
 var currentLocation;
 var locationIndex;
 var currentDate;
@@ -10,14 +10,11 @@ var map;
 var marker;
 var currentGeolocation = {};
 
-//current location string for current location
 var CURRENT_LOCATION_STR = 'Current Location';
 
-//function to build the location 
 var loadLocation = function()
 {
 	locationIndex = localStorage.getItem(APP_PREFIX + "-selectedLocation"); 
-    //function built to use cache details for a location
 	if (locationIndex !== null && locationIndex != -1)
 	{
 		currentLocation = locationWeatherCache.locationAtIndex(locationIndex);
@@ -32,43 +29,34 @@ var loadLocation = function()
 	    currentDate = new Date();
 		updateFields();
 	} 
-    //finding the current location
 	else if (locationIndex == -1) 
 	{
 		document.getElementById("headerBarTitle").textContent = CURRENT_LOCATION_STR;
 
 		// setup geolocation
-        //failsafe
 		if (!navigator.geolocation)
 		{
 		  alert("Geolocation is not supported by your browser");
 		}
-        //if success then it will use the geocode location to find your location 
 		var success = function(position, hasMap) 
 		{
-            //creating the logitude and latitude of the location
 		    var latitude  = parseFloat(position.coords.latitude);
 		    var longitude = parseFloat(position.coords.longitude);
 		    currentGeolocation.latitude = latitude;
 		    currentGeolocation.longitude = longitude;
 		    var locationPosition = {lat: latitude, lng: longitude};
-		   //failsafe
-            if (!hasMap) {
+		    if (!hasMap) {
 		    	buildMap(locationPosition);
-		    } 
-            //placing the center of the map at the intende location
-            else {
+		    } else {
 		    	changeCenter(locationPosition);
 		    }
-            
-            //adding curend location to index
+
 		    locationIndex = locationWeatherCache.addLocation(latitude, longitude, CURRENT_LOCATION_STR);
 
 		    currentDate = new Date();
 			updateFields();
 		};
-		
-		        //updating the current location
+
 		var watch_success = function(position)
 		{
 			var latitude  = parseFloat(position.coords.latitude);
@@ -81,14 +69,14 @@ var loadLocation = function()
 				success(position, true);
 			}
 		}
-        //failsafe
+
 		var error = function() 
 		{
 			alert("Unable to retrieve your location");
 		};
 
 		navigator.geolocation.getCurrentPosition(success, error);
-        //runtime limitation
+
 		var options = {
 		  enableHighAccuracy: true, 
 		  maximumAge        : 30000, 
@@ -101,15 +89,12 @@ var loadLocation = function()
 	}
 }
 
-//getting rid of the current position in the location index
 var clearCurrentPosition = function()
 {
-    //removes the location index in the cache
     locationWeatherCache.removeLocationAtIndex(locationIndex);
     locationIndex = -1;
 }
 
-//creating map from google api systems
 var buildMap = function(locationPosition)
 {
 	map = new google.maps.Map(document.getElementById('map'), {
@@ -122,7 +107,6 @@ var buildMap = function(locationPosition)
 	});
 }
 
-//changes the center to the location that is given in the location postion of the index
 var changeCenter = function(locationPosition)
 {
 	map.setCenter(locationPosition);
@@ -133,8 +117,6 @@ var changeCenter = function(locationPosition)
 	});
 }
 
-
-//updating the date with the current date as given by the slider bar
 var updateFields = function()
 {
 	updateCurrentDate();
@@ -143,71 +125,47 @@ var updateFields = function()
 
 var updateWeatherInfo = function() 
 {
-    //create id
 	var weatherSummaryEl = document.getElementById('weatherSummary');
-    //gives loading string as the information loads
 	weatherSummaryEl.innerHTML = '<span>Loading...</span>';
-    
-    //failsafe
+
 	if (locationIndex == -1) {
 		return;
 	}
 
-    //get weather for the location and date
 	locationWeatherCache.getWeatherAtIndexForDate(locationIndex, currentDate, updateWeatherSummary); //passes in callback function
 }
 
-//chaneges the current date into proper formate via locationweathercache
 var updateCurrentDate = function()
 {
 	document.getElementById('dateField').innerHTML = currentDate.simpleDateString();
 }
 
-
-
 var updateWeatherSummary = function(index, weatherData)
 {
-    //falesafe
 	if (!weatherData) {
 		// console.log("N/a weather data");
 		return;
 	}
-    //creats and id
 	var weatherSummaryEl = document.getElementById('weatherSummary');
-    //gets the written weather summary for the day
 	var weatherCondition = weatherData.data[0].summary;
-    //gets the minimum temperature after converting it from ferenheit to celcius which has the function on the location weather cache
 	var minTemp = farenheitToCelcius(weatherData.data[0].temperatureMin);
-    //gets the maximum temperature after converting it from ferenheit to celcius which has the function on the location weather cache
 	var maxTemp = farenheitToCelcius(weatherData.data[0].temperatureMax);
-    // get the humidity humidity percentage for the day to an accuracy 0f 0.1 decimals
 	var humidity = (weatherData.data[0].humidity * 100).toFixed(1);
-    // get the windspeed
 	var windSpeed = (weatherData.data[0].windSpeed / 0.62137).toFixed(1); //convert to km/h from mph
-	//summerise the information into one variable
-    weatherSummaryEl.innerHTML = '<span class="mdl-list__item-text-body">' + weatherCondition + '</br> <b>Min temperature:</b> ' + minTemp + '&deg;C</br><b>Max temperature:</b> ' + maxTemp + '&deg;C<br/><b>Humidity:</b> ' + humidity + '%</br><b>Wind speed:</b> ' + windSpeed + 'km/h</span>';
+	weatherSummaryEl.innerHTML = '<span class="mdl-list__item-text-body">' + weatherCondition + '</br> <b>Min temperature:</b> ' + minTemp + '&deg;C</br><b>Max temperature:</b> ' + maxTemp + '&deg;C<br/><b>Humidity:</b> ' + humidity + '%</br><b>Wind speed:</b> ' + windSpeed + 'km/h</span>';
 }
 
-//function to update the weather according to date
 var updateDate = function()
 {
-    //creation of ide
 	var sliderEl = document.getElementById('dateSlider');
-    //necessary subtraction amount to find desired date
-	var dateToSubtract = sliderEl.value - 30; 
-    //create a new date 
+	var dateToSubtract = sliderEl.value - 30; //necessary subtraction amount to find desired date
 	currentDate = new Date();
-    //get current data for past date
-    //subtracts despite plus sign due to negative dateToSubtract
-	currentDate.setDate(currentDate.getDate() + dateToSubtract); 
-    //update 
+	currentDate.setDate(currentDate.getDate() + dateToSubtract); //subtracts despite plus sign due to negative dateToSubtract
 	updateFields();
 }
 
-//function to delete lcation from location list
 var removeLocation = function()
 {
-    
 	locationWeatherCache.removeLocationAtIndex(locationIndex);
 	back();
 }
@@ -217,9 +175,4 @@ var back = function()
 	location.href = 'index.html';
 }
 
-//loads the location at the end
 loadLocations();
-
-
-
-
