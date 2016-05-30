0e6e6aec366961d63f4bf1b07c953bacb3dcdac2
markthomas1997 <metho8@student.monash.edu>
2016-05-30 14:21:09 +1000

    Update mainPage.js

--- mainPage.js
+++ mainPage.js
@@ -2,39 +2,93 @@
 
 // This is sample code to demonstrate navigation.
 // You need not use it for final app.
-locationsList = localStorage.getItem("addedLocations");
-var locationWeatherCache = new LocationWeatherCache(locationsList);
-function viewLocation(locationName)
+
+var viewLocation = function(locationName)
 {
     // Save the desired location to local storage
     localStorage.setItem(APP_PREFIX + "-selectedLocation", locationName); 
     // And load the view location page.
     location.href = 'viewlocation.html';
-} 
-
-/*for(var i = 0; i < locationWeatherCache.length(); i++){
-    var theDiv = document.getElementById("locationList");
-    var content = document.createTextNode('class=mdl-list__item mdl-list__item--two-line" onclick="viewLocation(1);">\
-                <span class="mdl-list__item-primary-content">\
-                  <img class="mdl-list__item-icon" id="icon1" src="images/loading.png" class="list-avatar" />\
-                  <span>Location B</span>\
-                  <span id="weather1" class="mdl-list__item-sub-title">Weather summary</span>\
-                </span>');
-    theDiv.appendChild(content);
 }
-*/
-//locationsList = locationWeatherCache.getStorage("addedLocations");
-var div = document.getElementById('locationList');
 
-//for(var i = 0; i < locationWeatherCache.getStorage().length(); i++){
-for(var i = 0; i < locationWeatherCache.getStorage().length; i++){
-    console.log("count");
-    div.innerHTML += '<ul class="mdl-list" id="locationList">\
-                             <li class="mdl-list__item mdl-list__item--two-line" onclick="viewLocation('+ i +')">\
-                             <span class="mdl-list__item-primary-content"><img class="mdl-list__item-icon" id="icon0"\ src="images/loading.png" class="list-avatar" />\
-                             <span>'+ locationWeatherCache.getStorage()[i].nick + '</span>\
-                             <span id="weather0" class="mdl-list__item-sub-title">Click here for weather information...</span></span></li>';
-
+var buildLocationLists = function() 
+{
+	var list = document.getElementById('locationList');
+	for (var i = 0; i < locationWeatherCache.length(); i++) {
+		var li = buildLocationDefaultListItem(i);
+		list.appendChild(li);
+		var date = new Date();
+		locationWeatherCache.getWeatherAtIndexForDate(i, date, updateWeather);
+	}
 }
-//localStorage.setItem("storedDiv", div.innerHTML);
-console.log("child should be appended");
+
+var updateWeather = function(index, weatherData) 
+{
+	// stop loading spinner
+	var spinner = document.getElementById('spinner' + index);
+	spinner.className = 'mdl-spinner mdl-spinner--single-color mdl-js-spinner';
+
+	// update icon
+	var icon = document.getElementById('icon' + index);
+	icon.setAttribute('src', 'images/' + weatherData.data[0].icon + '.png');
+
+	// update temparatures info
+	var temperatures = document.getElementById('weather' + index);
+	temperatures.innerHTML = 'Low: ' + farenheitToCelcius(weatherData.data[0].temperatureMin) + '&deg;C | ' + 'High: ' + farenheitToCelcius(weatherData.data[0].temperatureMax) + '&deg;C'; //ASCII code for degree sign
+}
+
+var buildLocationDefaultListItem = function(index) 
+{
+	// create li element
+	var li = document.createElement('li');
+	li.className = 'mdl-list__item mdl-list__item--two-line';
+	li.setAttribute('onClick', 'viewLocation(' + index +')');
+
+	// create li's child div element
+	var div = document.createElement('div');
+	div.className = 'mdl-spinner mdl-spinner--single-color mdl-js-spinner is-active';
+	div.setAttribute('id', 'spinner' + index);
+
+	// create li's child span element
+	var span = buildDefaultListItemChildSpan(index);
+
+	// appending child elements to li
+	li.appendChild(div);
+	li.appendChild(span);
+	return li;
+}
+
+var buildDefaultListItemChildSpan = function(index) 
+{
+	var location = locationWeatherCache.locationAtIndex(index);
+
+	// create span element
+	var span = document.createElement('span');
+	span.className = 'mdl-list__item-primary-content';
+
+	// create span's child img element
+	var childImg = document.createElement('img');
+	childImg.className = 'mdl-list__item-icon list-avatar';
+	childImg.setAttribute('id', 'icon' + index);
+	childImg.setAttribute('src', 'images/loading.png');
+
+	// create span's first child span element
+	var childSpan = document.createElement('span');
+	childSpan.setAttribute('id', 'locName' + index);
+	childSpan.innerHTML = location.nickname;
+
+	// create span's second child span element
+	var childSpan2 = document.createElement('span');
+	childSpan2.className = 'mdl-list__item-sub-title';
+	childSpan2.setAttribute('id', 'weather' + index);
+	childSpan2.innerHTML = 'Loading Low/High temperatures...';
+
+	// append child elements to span
+	span.appendChild(childImg);
+	span.appendChild(childSpan);
+	span.appendChild(childSpan2);
+	return span;
+}
+
+loadLocations();
+buildLocationLists();
